0af0e4b44bca1a660998ffdffb44a8ec0411f80d
Kelly King <kelly.danielle.king@gmail.com>
2013-09-29 23:28:16 -0400

    add some html

--- views/SearchView.php
+++ views/SearchView.php
@@ -1,2 +1,17 @@
 <?php include_once('views/global/Header.php'); ?>
-Hello from the search view
+<form role="form">
+  	<div class="form-group">
+	    <label for="searchFor">Search for:</label>
+	    <input type="text" class="form-control" id="searchFor" placeholder="ex. Databases">
+	</div> 
+  	<div class="form-group">
+	    <label for="category">Category:</label>
+	    <select multiple name="category" id="category">
+	    	<option value="compsci">Computer Science</option>
+	    	<option value="music">Music</option>
+	    	<option value="english">English</option>
+	    </select>
+  	</div>
+	  	
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
